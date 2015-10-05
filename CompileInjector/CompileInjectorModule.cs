21eebd1f804e57719f8b0da4e1860f100d9722a2
chris smith <chris.smith324@gmail.com>
2015-10-04 23:50:49 -0400

    Generate the method bodies programmatically.

--- CompileInjector/CompileInjectorModule.cs
+++ CompileInjector/CompileInjectorModule.cs
@@ -31,62 +31,130 @@ namespace CompileInjector
         public void BeforeCompile(BeforeCompileContext context)
         {
             //Debugger.Launch();
-
-            // Get our Greeter class.
-            var syntaxMatch = context.Compilation.SyntaxTrees
-                .Select(s => new
-                {
-                    Tree = s,
-                    Root = s.GetRoot(),
-                    Class = s.GetRoot().DescendantNodes().OfType<ClassDeclarationSyntax>().Where(cs => cs.Identifier.ValueText == "Greeter").SingleOrDefault()
-                })
-                .Where(a => a.Class != null)
-                .Single();
-
-            var tree = syntaxMatch.Tree;
-            var root = syntaxMatch.Root;
-            var classSyntax = syntaxMatch.Class;
-
-
-            var method = T.ParseText(@"
-public static Greeter Factory()
-{
-    return new Greeter(new MessageGreeter());
-}
-");
-
-            var rootFactory = method.GetRoot();
-
-            var factoryMethod = (MethodDeclarationSyntax)rootFactory.DescendantNodes().First();
             
-            var newClass = classSyntax.WithMembers(classSyntax.Members.Add(factoryMethod));
+            foreach(var syntaxTree in context.Compilation.SyntaxTrees)
+            {
+                var newTree = ModifyTree(syntaxTree);
 
-            //// Get the method declaration.
-            //var methodSyntax = classSyntax.Members
-            //    .OfType<MethodDeclarationSyntax>()
-            //    .Where(ms => ms.Identifier.ValueText == "GetMessage")
-            //    .Single();
+                // Replace the compilation.
+                context.Compilation = context.Compilation.ReplaceSyntaxTree(syntaxTree, newTree);
+            }
+        }
 
-            //// Let's implement the body.
-            //var returnStatement = F.ReturnStatement(
-            //    F.LiteralExpression(
-            //        K.StringLiteralExpression,
-            //        F.Literal(@"""weeeee!""")));
+        private SyntaxTree ModifyTree(SyntaxTree originalTree)
+        {
+            SyntaxTree newTree = originalTree;
 
-            //// Get the body block
-            //var bodyBlock = methodSyntax.Body;
+            while (true)
+            {
+                var node = GetFirstClassThatNeedsAFactory(newTree);
+                if(node == null)
+                {
+                    return newTree;
+                }
 
-            //// Create a new body block, with our new statement.
-            //var newBodyBlock = F.Block(new StatementSyntax[] { returnStatement });
+                var classSyntax = node.Class;
+                
+                var factoryMethod = GetFactoryMethodDeclaration(classSyntax);
+                
+                var newClass = classSyntax.WithMembers(classSyntax.Members.Add(factoryMethod));
+                
+                // Get the revised root
+                var newRoot = (CompilationUnitSyntax) node.Root.ReplaceNode(classSyntax, newClass);
+                
+                // Create a new syntax tree.
+                newTree = T.Create(newRoot);
+            }
+        }
 
-            // Get the revised root
-            var newRoot = (CompilationUnitSyntax)root.ReplaceNode(classSyntax, newClass);
+        private Node GetFirstClassThatNeedsAFactory(SyntaxTree syntaxTree)
+        {
+            var root = syntaxTree.GetRoot();
+            var classDeclarations = root.DescendantNodes().OfType<ClassDeclarationSyntax>()
+                .Where(node => node.Identifier.ValueText == "Greeter"
+                    || node.Identifier.ValueText == "MessageGreeter");
+            
+            foreach(var classDeclaration in classDeclarations)
+            {
+                var methods = classDeclaration.DescendantNodes().OfType<MethodDeclarationSyntax>().ToList();
+                if (!methods.Where(d => d.Identifier.Text == "Factory").Any())
+                {
+                    return new Node
+                    {
+                        Tree = syntaxTree,
+                        Root = root,
+                        Class = classDeclaration,
+                    };
+                }
+            }
 
-            // Create a new syntax tree.
-            var newTree = T.Create(newRoot);
+            return null;
+        }
 
-            // Replace the compilation.
-            context.Compilation = context.Compilation.ReplaceSyntaxTree(tree, newTree);
+        private class Node
+        {
+            public SyntaxTree Tree { get; set; }
+            public SyntaxNode Root { get; set; }
+            public ClassDeclarationSyntax Class { get; set; }
+        }
+
+        private static MethodDeclarationSyntax GetFactoryMethodDeclaration(ClassDeclarationSyntax @class)
+        {
+            return
+                F.MethodDeclaration(
+                    F.ParseName(@class.Identifier.Text),
+                    F.Identifier(F.TriviaList(F.Space), @"Factory", F.TriviaList()))
+                    .WithModifiers(F.TokenList(
+                        F.Token(F.TriviaList(), K.PublicKeyword, F.TriviaList(F.Space)),
+                        F.Token(F.TriviaList(), K.StaticKeyword, F.TriviaList(F.Space))))
+                    .WithBody(
+                        F.Block(F.SingletonList<StatementSyntax>(
+                            F.ReturnStatement(CallConstuctor(@class))
+                            .WithReturnKeyword(
+                                F.Token(
+                                    F.TriviaList(
+                                        F.Whitespace(
+                                            "   ")),
+                                    K.ReturnKeyword,
+                                    F.TriviaList(
+                                        F.Space)))
+                            .WithSemicolonToken(
+                                F.Token(
+                                    F.TriviaList(),
+                                    K.SemicolonToken,
+                                    F.TriviaList()
+                                ))
+                    )));
+        }
+
+        private static ObjectCreationExpressionSyntax CallConstuctor(ClassDeclarationSyntax @class)
+        {
+            var typeName = F.ParseName(@class.Identifier.Text);
+            var args = F.ArgumentList();
+
+            var ctor = @class.DescendantNodes().OfType<ConstructorDeclarationSyntax>().FirstOrDefault();
+
+            if(ctor != null)
+            {
+                var parameters = ctor.ParameterList.DescendantNodes().OfType<ParameterSyntax>().ToList();
+
+                foreach (var parameter in parameters)
+                {
+                    var ident = parameter.DescendantNodes().OfType<IdentifierNameSyntax>().Single();
+                    var parameterType = ident.Identifier.Text;
+
+                    var invocation = F.InvocationExpression(F.MemberAccessExpression(
+                        K.SimpleMemberAccessExpression,
+                        F.IdentifierName(parameterType),
+                        F.IdentifierName("Factory")));
+
+                    args = args.AddArguments(F.Argument(invocation));
+                }
+            }
+            
+            return F.ObjectCreationExpression(typeName)
+                    .WithNewKeyword(F.Token(F.TriviaList(), K.NewKeyword, F.TriviaList(F.Space)))
+                    .WithArgumentList(args);
         }
     }
 }
